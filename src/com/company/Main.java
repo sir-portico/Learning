580dee5b3d44f92c390ce42a87b06e48f4c2b2b6
sir-portico <logan1033@gmail.com>
2015-03-17 16:19:41 -0700

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I synced this across computers finally!");
+        System.out.println("It only worked now!");
     }
 }
